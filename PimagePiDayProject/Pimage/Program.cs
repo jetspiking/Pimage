4638da443b7ad01f0a8611f5d51518d975a190bd
Dustin <43465192+jetspiking@users.noreply.github.com>
2021-03-14 01:19:09 +0100

    Add files via upload

--- PimagePiDayProject/Pimage/Program.cs
+++ PimagePiDayProject/Pimage/Program.cs
@@ -25,38 +25,43 @@ namespace Pimage
             String height = Console.ReadLine();
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Working on your image, please wait a few Piconds :) ...");
+
+            int fontSizeInt = int.Parse(fontSize);
+            int widthInt = int.Parse(width);
+            int widthHeight = int.Parse(height);
+
             try
             {
-                int fontSizeInt = int.Parse(fontSize);
-                int widthInt = int.Parse(width);
-                int widthHeight = int.Parse(height);
-
-                try
-                {
-                    if (Pimage.ToPimage(_rootFolder, fontSizeInt, widthInt, widthHeight))
-                    {
-                        Console.ForegroundColor = ConsoleColor.Green;
-                        Console.WriteLine("Succeeded! Image is named \"pimage_result.png\"");
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = ConsoleColor.Red;
-                        Console.WriteLine("Make sure you have a source image named \"pimage.png\" available in the same directory as this executable. Terminating...");
-                        Console.Read();
-                        Environment.Exit(-1);
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.ForegroundColor = ConsoleColor.Red;
-                    Console.WriteLine("Your desired resolution is too high, attempt a smaller resolution. Terminating...");
-                    Console.Read();
-                    Environment.Exit(-1);
-                }
-            } catch(Exception e)
+                fontSizeInt = int.Parse(fontSize);
+                widthInt = int.Parse(width);
+                widthHeight = int.Parse(height);
+            }
+            catch (Exception e)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Input must be numeric, terminating...");
+                Console.Read();
+                Environment.Exit(-1);
+            }
+            try
+            {
+                if (Pimage.ToPimage(_rootFolder, fontSizeInt, widthInt, widthHeight))
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    Console.WriteLine("Succeeded! Image is named \"pimage_result.png\"");
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Make sure you have a source image named \"pimage.png\" available in the same directory as this executable. Terminating...");
+                    Console.Read();
+                    Environment.Exit(-1);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Your desired resolution is too high, attempt a smaller resolution. Terminating...");
                 Console.Read();
                 Environment.Exit(-1);
             }
